73c7bae7d9ccaf94416f7d1b167ed1e06a65d8d9
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-06-17 21:06:40 +0530

    longest substring

--- Sliding Window/longestSubString.java
+++ Sliding Window/longestSubString.java
@@ -1,12 +1,14 @@
 // longest substring without repeating characters
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class longestSubString {
     public static void main(String[] args) {
         String s = "abcabcbb";
         System.out.println(longest(s));
+        System.out.println(longestsubstring(s));
     }
 
     public static int longest(String s) {
@@ -21,6 +23,24 @@ public class longestSubString {
             map.put(right, windowEnd);
             max = Math.max(max, windowEnd - windowStart + 1);
         }
+        // System.out.println(map);
         return max;
     }
+
+public static int longestsubstring(String s){
+    int n = s.length();
+    int[] charIndex = new int[128];
+    int maxLength = 0;
+    int start = 0;
+    for (int end = 0; end < n; end++) {
+        char c = s.charAt(end);
+        if (charIndex[c] > start) {
+            start = charIndex[c];
+        }
+        charIndex[c] = end + 1;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+    // System.out.println(Arrays.toString(charIndex));
+    return maxLength;
+}
 }
