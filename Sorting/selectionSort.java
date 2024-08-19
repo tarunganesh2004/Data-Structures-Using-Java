7ce71d12b9854dd01a4fdb26648bf613c3e78bf5
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-08-19 19:29:57 +0530

    selection sort

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -4,19 +4,21 @@ public class selectionSort {
     public static void main(String[] args) {
         int[] a = { 210, 523, 124, 700, 158, 923, 112, 350 };
         selection(a);
+        System.out.println(Arrays.toString(a));
     }
 
     public static void selection(int[] a) {
         int n = a.length;
         for (int i = 0; i < n - 1; i++) {
+            int min = i;
             for (int j = i + 1; j < n; j++) {
-                if (a[i] > a[j]) {
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+                if (a[j] < a[min]) {
+                    min = j;
                 }
             }
+            int temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
         }
-        System.out.println(Arrays.toString(a));
     }
 }
